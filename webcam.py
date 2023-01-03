484be998c06881f5582b76db98c55a531c343f02
James Wickenden <jameswickenden14@gmail.com>
2023-01-03 17:58:01 +0000

    webpage now working locally

--- webcam.py
+++ webcam.py
@@ -1,13 +1,53 @@
+from flask import Flask, render_template, Response
 import cv2
 
-cam = cv2.VideoCapture(0)
+# Initialize the Flask app and camera object
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
 
-while True:
-	ret, image = cam.read()
-	cv2.imshow('Imagetest',image)
-	k = cv2.waitKey(1)
-	if k != -1:
-		break
-cv2.imwrite('testimage.jpg', image)
-cam.release()
-cv2.destroyAllWindows()
+
+# Flask index page route
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+def resize_frame(frame):
+	scale_percent = 100 # percent of original size
+	width = int(frame.shape[1] * scale_percent / 100)
+	height = int(frame.shape[0] * scale_percent / 100)
+	dim = (width, height)
+
+	resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+	return resized
+
+
+def generate_frames():
+	while True:
+		success, frame = camera.read()
+		frame = resize_frame(frame)
+		if not success:
+			camera.release()
+			cv2.destroyAllWindows()
+			break
+		else:
+			ret, buffer = cv2.imencode('.jpg', frame)
+			frame = buffer.tobytes()
+			yield (b'--frame\r\n'
+				   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+		
+		k = cv2.waitKey(1)
+		if k != -1:
+			# cv2.imwrite('testimage.jpg', frame)
+			camera.release()
+			cv2.destroyAllWindows()
+			break
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

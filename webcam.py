4553ebd4c1dfe14cd796f89e9f4a118255c29833
James Wickenden <jameswickenden14@gmail.com>
2023-01-04 15:16:16 +0000

    working fix

--- webcam.py
+++ webcam.py
@@ -4,7 +4,8 @@ import cv2
 # Initialize the Flask app and camera object
 app = Flask(__name__)
 camera = cv2.VideoCapture(0)
-
+print(type(camera))
+print(camera.isOpened())
 
 # Flask routes
 @app.route('/')
@@ -17,30 +18,30 @@ def video_feed():
 
 # Unused
 def resize_frame(frame, percent_to_reduce_to):
-	scale_percent = percent_to_reduce_to # percent of original size
-	width = int(frame.shape[1] * scale_percent / 100)
-	height = int(frame.shape[0] * scale_percent / 100)
-	dim = (width, height)
+        scale_percent = percent_to_reduce_to # percent of original size
+        width = int(frame.shape[1] * scale_percent / 100)
+        height = int(frame.shape[0] * scale_percent / 100)
+        dim = (width, height)
 
-	resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
-	return resized
+        resized = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        return resized
 
 
 # Generate a buffered frame for /video_feed
 def generate_frames():
-	while True:
-		success, frame = camera.read()
-		#frame = resize_frame(frame, 100)
-		if not success:
-			camera.release()
-			cv2.destroyAllWindows()
-			break
-		else:
-			ret, buffer = cv2.imencode('.jpg', frame)
-			frame = buffer.tobytes()
-			yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        while True:
+                success, frame = camera.read()
+                #frame = resize_frame(frame, 100)
+                if not success:
+                        camera.release()
+                        cv2.destroyAllWindows()
+                        break
+                else:
+                        ret, buffer = cv2.imencode('.jpg', frame)
+                        frame = buffer.tobytes()
+                        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 
 # Run main
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host="0.0.0.0")
